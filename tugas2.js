a2fcc7640e5c3e4b09deb984034425e84bc4f523
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-01 16:56:42 +0700

    add file tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number = 1;
+var string = "variabel string";
+var boolean = true;
+var kosong = null;
+var array = [2,3,4,5,6];
+
+console.log("ini adalah tipe data number: ", number);
+console.log("ini adalah tipe data string: ", string);
+console.log("ini adalah tipe data boolean: ", boolean);
+console.log("ini adalah tipe data null: ", kosong);
+console.log("ini adalah tipe data array: ", array);

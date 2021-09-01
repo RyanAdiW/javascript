b96380025788ce03cbf106826b92b9eb353e2170
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-01 16:12:15 +0700

    belajar print console, var, let, const

---
+++ index.js
@@ -0,0 +1,14 @@
+var message = "Hello Coding";
+let msg1 = "Hello";
+
+const msg2 = "coding";
+
+if (true) {
+	var message = 1;
+	let msg1 = 2;
+	console.log("let dalam if/else", msg1);
+}
+
+console.log(message);
+console.log(msg1);
+console.log(msg2);

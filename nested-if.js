75581b656bbf1fbdde1f79a1862cd5fffd8e8aa2
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-06 10:08:00 +0700

    belajar nested-if

---
+++ nested-if.js
@@ -0,0 +1,19 @@
+var x = 10;
+var y = 20;
+var z = 30;
+
+if (x>y){
+	if(x>z){
+		console.log("x adalah yang terbesar");
+	}else{
+		console.log("x adalah terbesar kedua");
+	}
+}
+
+else{
+	if(x<y){
+		console.log("x adalah yang terkecil");
+	}else{
+		console.log("x adalah terkecil kedua");
+	}
+}

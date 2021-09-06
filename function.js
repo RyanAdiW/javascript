2a6749574988fb155e5e170f31ffc403b97ef04b
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-06 08:56:41 +0700

    belajar function

---
+++ function.js
@@ -0,0 +1,18 @@
+// penulisan function model lama
+function jalanSatu(){
+	console.log("Jalan satu");
+}
+
+const jalanDua = function(){
+	console.log("Jalan dua");
+}
+
+jalanSatu();
+jalanDua();
+
+// penulisan function model baru
+const jalanTiga = ()=>{
+	console.log("Jalan tiga")
+}
+
+jalanTiga();

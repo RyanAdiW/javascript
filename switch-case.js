483f798ee091f80fd69b350742c7463950342369
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-06 10:31:25 +0700

    belajar switch-case

---
+++ switch-case.js
@@ -0,0 +1,19 @@
+function testingSwitch(){
+	var nilai = 8;
+
+	switch(nilai){
+		case 10:
+			console.log("sangat bagus");
+		break;
+		case 5:
+			console.log("kurang, tingkatkan lagi");
+		break;
+		case 0:
+			console.log("sangat kurang, belajar yang bener!");
+		break;
+		default:
+			console.log("lainnya");
+	}
+}
+
+testingSwitch();

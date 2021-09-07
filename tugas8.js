a70361488f1259e63333fedc4b895e37473ea6b3
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-07 11:47:44 +0700

    add tugas8 js - object

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+var hewan = {
+	mamalia: ["gajah", "jerapah", "kerbau"],
+	aves: ["kalkun", "ayam", "burung"],
+	pisces: "cupang",
+	amfibi: "kodok",
+	reptil: "buaya"
+}
+
+console.log(hewan);
+
+hewan.reptil = "ular";
+console.log(hewan);

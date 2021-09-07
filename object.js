de54ebee4c81e0a60b4f1c993785b3284efcfa4c
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-07 11:35:20 +0700

    add array didalam object

--- object.js
+++ object.js
@@ -1,11 +1,16 @@
+// syntax dasar
 function panggilObjects(){
 	var mobil = {
 		type: "sedan",
 		harga: 500000000,
 		warna: 'putih',
+		tahun: [2010,2011,2018,2020]
 	}
 
 	console.log(mobil.harga);
+
+	// pemanggilan elemen array dalam objecct
+	console.log(mobil.tahun[0]);
 }
 
 panggilObjects();

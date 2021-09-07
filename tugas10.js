5f36468e525ed162dc14f8b40179d1d1a18bfb25
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-07 15:48:09 +0700

    add tugas 10 niomic

---
+++ tugas10.js
@@ -0,0 +1,14 @@
+/*==========================
+  tugas 10 javascript niomic
+  ==========================*/
+
+var a=0;
+
+for(var i = 0; i <= 10; i++){
+	var b=a+i
+	a=b;
+	
+	if (a !== 0){
+		console.log(a);
+	}
+}

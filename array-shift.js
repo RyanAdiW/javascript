791616bdbd62d50b1401b9192309696111f452a6
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-09-22 12:54:42 +0700

    add fungsi pop

--- array-shift.js
+++ array-shift.js
@@ -1,3 +1,4 @@
+//  shift untuk menghapus data array paling depan
 function panggilShift() {
 	var kota = ["jakarta", "bandung", "malang", "surabaya", "makasar"];
 	console.log(kota);
@@ -10,3 +11,19 @@ function panggilShift() {
 }
 
 console.log(panggilShift());
+
+console.log("=====================================================")
+
+// pop untuk menghapus data array paling belakang
+function panggilPop() {
+	var kota = ["jakarta", "bandung", "malang", "surabaya", "makasar"];
+	console.log(kota);
+	console.log("=========");
+
+	var kota2 = kota.pop();
+	console.log(kota2);
+
+	return kota;
+}
+
+console.log(panggilPop());

4406c56171d68da679380d4af28b496fd83cfeb6
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-10-01 15:53:39 +0700

    add tugas14.js

---
+++ tugas14.js
@@ -0,0 +1,8 @@
+function pembagian() {
+	var angka = 37;
+	return hasil = angka / 21;
+}
+
+console.log("Pembulatan round: ", Math.round(pembagian()));
+console.log("Pembulatan ceil: ", Math.ceil(pembagian()));
+console.log("Pembulatan floor: ", Math.floor(pembagian()));

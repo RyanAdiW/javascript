22daed65b0bd61e64218fa880d894f48522a1cad
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-10-01 17:07:03 +0700

    edit tugas17.js

--- tugas17.js
+++ tugas17.js
@@ -5,3 +5,16 @@ console.log(kalimat1.toUpperCase());
 console.log(kalimat1.toLowerCase());
 console.log(kalimat2.toUpperCase());
 console.log(kalimat2.toLowerCase());
+
+console.log("================================")
+
+var kalimat3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+var codeAscii0 = kalimat3.charCodeAt(0);
+var codeAscii1 = kalimat3.charCodeAt(1);
+var codeAscii2 = kalimat3.charCodeAt(2);
+var codeAscii3 = kalimat3.charCodeAt(3);
+var codeAscii4 = kalimat3.charCodeAt(4);
+var codeAscii5 = kalimat3.charCodeAt(5);
+
+console.log(codeAscii0,"-",codeAscii1,"-",codeAscii2,"-",codeAscii3,"-",codeAscii4,"-",codeAscii5);

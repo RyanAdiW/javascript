69953db6a1a3253b86be8e5ffad505ee68fcd965
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-10-01 16:23:13 +0700

    add tugas15.js

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+var ab = 8;
+var bc = 6;
+var ac;
+
+var acKuadrat = Math.pow(ab, 2) + Math.pow(bc, 2);
+
+ac = Math.sqrt(acKuadrat);
+
+console.log("Nilai AB: ", ab);
+console.log("Nilai BC: ", bc);
+console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah: ", ac);

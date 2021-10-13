cb79716316f19e2a0d3756954db1727aa241389a
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-10-13 08:47:31 +0700

    add function array-concat

--- array.js
+++ array.js
@@ -1,8 +1,17 @@
 // array-reverse (untuk membalik array)
-function panggilReverse(argument) {
+function panggilReverse() {
 	var kota = ["jakarta", "bandung", "temanggung", "yogyakarta"];
-	console.log(kota)
-	console.log(kota.reverse())
+	console.log(kota);
+	console.log(kota.reverse());
+}
+
+// array-concat (untuk menggabungkan 2 array)
+function panggilConcat() {
+	var kota1 = ["jakarta", "bandung", "temanggung", "yogyakarta"];
+	var kota2 = ["surabaya", "depok", "magelang", "solo"];
+
+	console.log(kota1.concat(kota2))
 }
 
 panggilReverse();
+panggilConcat();

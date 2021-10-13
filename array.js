d7ce06f9ba1fb4525472c64d13846ad7c08a5702
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-10-13 07:38:07 +0700

    add array.js (array reverse)

---
+++ array.js
@@ -0,0 +1,8 @@
+// array-reverse (untuk membalik array)
+function panggilReverse(argument) {
+	var kota = ["jakarta", "bandung", "temanggung", "yogyakarta"];
+	console.log(kota)
+	console.log(kota.reverse())
+}
+
+panggilReverse();

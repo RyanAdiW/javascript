a3c859512ad2cd3e01f7bce1d5a4db2f554e5c8b
Muhamad Ryan Adi Wiguna <ryan.adi911@gmail.com>
2021-10-14 14:01:47 +0700

    add contoh dari niomic

--- filter-array.js
+++ filter-array.js
@@ -1,3 +1,25 @@
+let peoples = [
+	{
+		name: "Andi",
+		gender: "male"
+	},
+	{
+		name: "Siti",
+		gender: "female"
+	},
+	{
+		name: "Cindy",
+		gender: "female"
+	}
+];
+
+let female = peoples.filter(item =>{
+	return item.gender === "female";
+})
+
+console.log(female);
+
+// contoh di w3school
 var umur = ['16','15','22','24','25'];
 
 function umurDewasa(umur) {
